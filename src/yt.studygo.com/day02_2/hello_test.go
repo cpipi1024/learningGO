944f02a9f3d0aadcbeea5b198fddd451070bdfa2
yangtao <1461481767@qq.com>
2022-01-11 00:39:30 +0800

    day02

--- src/yt.studygo.com/day02_2/hello_test.go
+++ src/yt.studygo.com/day02_2/hello_test.go
@@ -3,18 +3,34 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	assertMessage := func(t *testing.T, got, want string) {
+		t.Helper() // 告诉测试套件当前这个方法是辅助函数，当报错时报错的行数就会在函数调用中而不是辅助函数中
+		if got != want {
+			t.Errorf("got: '%q' want: '%q'", got, want)
+		}
+	}
+	// 测试用例 1
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := hello("chris")
+		got := hello("chris", "")
 		want := "Hello, chris"
-		if got != want {
-			t.Errorf("got: '%q' want: '%q'", got, want)
-		}
+		assertMessage(t, got, want)
 	})
+	// 测试用例 2
 	t.Run("saying hello to empty", func(t *testing.T) {
-		got := hello("")
+		got := hello("", "")
 		want := "Hello, world"
-		if got != want {
-			t.Errorf("got: '%q' want: '%q'", got, want)
-		}
+		assertMessage(t, got, want)
+	})
+	// 测试用例 3
+	t.Run("saying in spanish", func(t *testing.T) {
+		got := hello("elodie", "spanish")
+		want := "Hola, elodie"
+		assertMessage(t, got, want)
+	})
+	// 测试用例 4
+	t.Run("saying in franch", func(t *testing.T) {
+		got := hello("elodie", "franch")
+		want := "Bonjour, elodie"
+		assertMessage(t, got, want)
 	})
 }
--- src/yt.studygo.com/day02_2/main.go
+++ src/yt.studygo.com/day02_2/main.go
@@ -1,15 +1,26 @@
 package main
 
-import (
-	"fmt"
-)
-
 const englishPreFix = "Hello, "
+const spanishPreFix = "Hola, "
+const franchPreFix = "Bonjour, "
 
-func hello(name string) string {
-	return englishPreFix + name
+func hello(name string, language string) string {
+	if name == "" {
+		name = "world"
+	}
+	return getLanguage(language) + name
+}
+func getLanguage(language string) (prefix string) {
+	switch language {
+	case "spanish":
+		prefix = spanishPreFix
+	case "franch":
+		prefix = franchPreFix
+	default:
+		prefix = englishPreFix
+	}
+	return prefix
 }
 
 func main() {
-	fmt.Println(hello("chris"))
 }
