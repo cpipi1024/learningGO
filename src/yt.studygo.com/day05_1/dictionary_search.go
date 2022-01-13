7e590ad58b2d3331e73ad8b58f3101c07e829ce4
yangtao <1461481767@qq.com>
2022-01-14 00:27:06 +0800

    map学习还剩一半

--- src/yt.studygo.com/day05_1/dictionary_search.go
+++ src/yt.studygo.com/day05_1/dictionary_search.go
@@ -1,5 +1,31 @@
 package dictionary
 
-func Search(dictionary map[string]string, key string) string {
+import "errors"
+
+//使用自定义dictionary类型
+
+type Dictionary map[string]string
+
+//使用自定义错误类型
+
+var ErrorNotFound = errors.New("could not found a value")
+
+/* func Search(dictionary map[string]string, key string) string {
 	return dictionary[key]
+} */
+
+// 实现自定义类的方法
+
+func (d Dictionary) Search(key string) (string, error) {
+	//return d[key], nil
+	// 重构抛出错误
+	definition, ok := d[key]
+	if !ok {
+		return "", ErrorNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(key, value string) {
+	d[key] = value
 }
--- src/yt.studygo.com/day05_1/dictionary_test.go
+++ src/yt.studygo.com/day05_1/dictionary_test.go
@@ -5,13 +5,54 @@ import (
 )
 
 func TestDictionary(t *testing.T) {
-	dict := map[string]string{
+	dict := Dictionary{
 		"test": "this is a test",
 	}
-	got := Search(dict, "test")
-	want := "this is a test"
+	t.Run("有对应键值: ", func(t *testing.T) {
+		got, _ := dict.Search("test")
+		want := "this is a test"
+
+		assertString(t, got, want)
+	})
+
+	// 找不到键值的情况
+	t.Run("没有对应键值: ", func(t *testing.T) {
+		_, err := dict.Search("unknown")
+
+		if err == nil {
+			t.Fatal("expected a error")
+		}
+		//assertString(t, err.Error(), ErrorNotFound)
+		assertError(t, err, ErrorNotFound)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	dict := Dictionary{}
+	dict.Add("test", "this is also a test")
+	got, err := dict.Search("test")
+	want := "this is also a test"
+	if err != nil {
+		t.Fatal("should find a added value: ", err)
+	}
+
+	if got != want {
+		t.Errorf("got : %s want: %s", got, want)
+	}
+}
+
+func assertString(t *testing.T, got string, want string) {
+	t.Helper()
 
 	if want != got {
 		t.Errorf("got: %s want: %s", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, want error) { //为啥这里的got形参不用定义类型？
+	t.Helper()
+
+	if want != got {
+		t.Errorf("got error: %s want error: %s", got, want)
 	}
 }
