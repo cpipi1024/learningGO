29d2a2e5084cc4d27e265378df9dfe3ed31aaa76
yangtao <1461481767@qq.com>
2022-01-15 17:37:10 +0800

    map学习下半部分

--- src/yt.studygo.com/day05_1/dictionary_search.go
+++ src/yt.studygo.com/day05_1/dictionary_search.go
@@ -1,21 +1,36 @@
 package dictionary
 
-import "errors"
-
 //使用自定义dictionary类型
 
 type Dictionary map[string]string
 
-//使用自定义错误类型
+// 创建错误类型
+//var ErrorNotFound = errors.New("could not found a value")
 
-var ErrorNotFound = errors.New("could not found a value")
-
+/*var (
+	ErrorNotFound = errors.New("could not found a value")
+	ErrorExists   = errors.New("key already exists")
+)*/
+//
 /* func Search(dictionary map[string]string, key string) string {
 	return dictionary[key]
 } */
 
-// 实现自定义类的方法
+// 创建自定义错误类型
+type DictionaryErr string
 
+var (
+	ErrorNotFound     = DictionaryErr("could not found a value")
+	ErrorExists       = DictionaryErr("key already exists")
+	ErrorKeyNotExists = DictionaryErr("could not update key not exists")
+)
+
+// 实现自定义类型返回接口
+func (de DictionaryErr) Error() string {
+	return string(de)
+}
+
+// 实现自定义类的方法
 func (d Dictionary) Search(key string) (string, error) {
 	//return d[key], nil
 	// 重构抛出错误
@@ -26,6 +41,29 @@ func (d Dictionary) Search(key string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(key, value string) {
-	d[key] = value
+func (d Dictionary) Add(key, value string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrorNotFound:
+		d[key] = value
+	case nil:
+		return ErrorExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(key, value string) error {
+	_, err := d.Search(key)
+	switch err {
+	case ErrorNotFound:
+		return ErrorKeyNotExists
+	case nil:
+		d[key] = value
+	default:
+		return err
+	}
+	return nil
 }
--- src/yt.studygo.com/day05_1/dictionary_test.go
+++ src/yt.studygo.com/day05_1/dictionary_test.go
@@ -28,17 +28,55 @@ func TestDictionary(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dict := Dictionary{}
-	dict.Add("test", "this is also a test")
-	got, err := dict.Search("test")
-	want := "this is also a test"
-	if err != nil {
-		t.Fatal("should find a added value: ", err)
-	}
+	t.Run("添加不存在的键值", func(t *testing.T) {
+		dict := Dictionary{}
+		dict.Add("unknown", "this is also a test")
+		got, err := dict.Search("unknown")
+		want := "this is also a test"
+		if err != nil {
+			t.Fatal("should found a added value: ", err)
+		}
 
-	if got != want {
-		t.Errorf("got : %s want: %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got : %s want: %s", got, want)
+		}
+	})
+	t.Run("添加已存在键值", func(t *testing.T) {
+		//dict := Dictionary{}
+		word := "test"
+		definition := "this is a new test!"
+
+		dict := Dictionary{word: definition}
+
+		err := dict.Add(word, "new test!")
+		assertError(t, err, ErrorExists)
+		assertDefinition(t, dict, word, definition)
+
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("更新存在的键值", func(t *testing.T) {
+		word := "test"
+		definition := "this is a test"
+		dict := Dictionary{word: definition}
+
+		new_definition := "new test"
+
+		dict.Update(word, new_definition)
+
+		assertDefinition(t, dict, word, new_definition)
+	})
+
+	t.Run("更新不存在的键值", func(t *testing.T) {
+		word := "unknown"
+		definition := "unknown value "
+		dict := Dictionary{}
+
+		err := dict.Update(word, definition)
+		assertError(t, err, ErrorKeyNotExists)
+		//assertDefinition(t, dict, word, definition)
+	})
 }
 
 func assertString(t *testing.T, got string, want string) {
@@ -49,10 +87,25 @@ func assertString(t *testing.T, got string, want string) {
 	}
 }
 
-func assertError(t *testing.T, got, want error) { //为啥这里的got形参不用定义类型？
+func assertError(t *testing.T, got, want error) { //为啥这里的got形参不用定义类型？ 参数两个一样类型可以指声明一次
 	t.Helper()
 
 	if want != got {
 		t.Errorf("got error: %s want error: %s", got, want)
 	}
+}
+
+// 新的断言 为已存在键的map重新赋值
+
+func assertDefinition(t *testing.T, dict Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dict.Search(word)
+	if err != nil {
+		t.Fatal("should found a added value: ", err)
+	}
+	if definition != got {
+		t.Errorf("got: %s want: %s", got, definition)
+	}
+
 }
